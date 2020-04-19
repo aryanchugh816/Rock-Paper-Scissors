4ff23b80d4d609cc428482d44d3d11b048425ead
Aryan Chugh <chugharyan816@gmail.com>
2020-04-20 01:09:27 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Rock-Paper-Scissors 
+opencv-python==3.2.0.8 works only
--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def index():
     return render_template("index2.html")
 
 if __name__ == "__main__":
-    app.run(, debug=True)
+    app.run(debug=True)
