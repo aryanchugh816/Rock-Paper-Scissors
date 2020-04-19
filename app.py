2834bb0f4fa1a21e4e1501ccbb9d3c59c0411efb
Aryan Chugh <chugharyan816@gmail.com>
2020-04-20 00:33:53 +0530

    first commit

--- app.py
+++ app.py
@@ -13,24 +13,21 @@ def gen():
     """Video streaming generator function."""
     cap = cv2.VideoCapture(0)
 
-    if not cap.isOpened():
-        alert(text="Couldn't Open Web Cam", title="Error", button="OK")
-        exit()
-
     # Read until video is completed
     while(cap.isOpened()):
       # Capture frame-by-frame
         ret, img = cap.read()
 
-        if not ret:
-            alert(text="Can't receive frame (stream end?). Exiting ...", title="Error", button="OK")
-            exit()
+        if ret:
             
-        img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
-        img = cv2.flip(img, 1)
-        frame = cv2.imencode('.jpg', img)[1].tobytes()
-        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        plt.pause(0.1)
+            img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
+            img = cv2.flip(img, 1)
+            frame = cv2.imencode('.jpg', img)[1].tobytes()
+            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            plt.pause(0.1)
+
+        else:
+            break
 
 
 @app.route('/video_feed')
