2d31f500c2806826c00db7f36d30d31c8af1467e
Aryan Chugh <chugharyan816@gmail.com>
2020-04-19 21:16:42 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, Response
+import cv2, time
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+def gen():
+    """Video streaming generator function."""
+    cap = cv2.VideoCapture(0)
+
+    if not cap.isOpened():
+        alert(text="Couldn't Open Web Cam", title="Error", button="OK")
+        exit()
+
+    # Read until video is completed
+    while(cap.isOpened()):
+      # Capture frame-by-frame
+        ret, img = cap.read()
+
+        if not ret:
+            alert(text="Can't receive frame (stream end?). Exiting ...", title="Error", button="OK")
+            exit()
+            
+        img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
+        img = cv2.flip(img, 1)
+        frame = cv2.imencode('.jpg', img)[1].tobytes()
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        time.sleep(0.1)
+
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
