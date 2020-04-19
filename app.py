17e6b3a50a8327466643fd5a2e2b2413c8d05bdb
Aryan Chugh <chugharyan816@gmail.com>
2020-04-19 23:00:59 +0530

    first commit

--- app.py
+++ app.py
@@ -34,8 +34,9 @@ def gen():
 
 """
 @app.route('/video_feed')
-def video_feed():
+def video_feed():"""
     """Video streaming route. Put this in the src attribute of an img tag."""
+    """
     return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
 """
 
