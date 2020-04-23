5f9d0668e9e18c8a3dce075d760d35343f65d0df
Aryan Chugh <chugharyan816@gmail.com>
2020-04-23 18:25:57 +0530

    23.04.2020

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
-from flask import Flask, render_template
-import cv2, time
+from flask import Flask, render_template, request
+import cv2, time, json
+import numpy as np
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    print("Working")
-    while True:
-        keypress = cv2.waitKey(1) & 0xFF
-        if keypress == ord("q"):
-            print("pressed")
-    #return render_template("index2.html", cap="no")
+    
+    return render_template("index2.html")
+
+@app.route('/receive', methods=['POST'])
+def receive_data():
+    if request.method == 'POST':
+        data = request.get_json()
+        data = json.loads(data)
+        img = np.array(list(data["data"].values()))
+        img = np.asarray(img, dtype=np.uint8)
+        img = img.reshape(120,160,4)
+        img = cv2.cvtColor(img, cv2.COLOR_RGBA2GRAY)
+        cv2.imwrite("received_img.jpg", img)
+        print("Working 1")
+        print(img.shape)
+        print("Working 2")
+    return ' '
 
 if __name__ == "__main__":
     app.run(debug=True)
